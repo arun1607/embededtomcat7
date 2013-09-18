f1e95371e773e873c92da1d038371a03529c3f6b
Amit Shrivastava <arun160782@gmail.com>
2013-09-18 14:47:24 +0530

    Initial commit

---
+++ embeded-tomcat-7/src/main/java/com/app/learning/execution/ServerLauncher.java
@@ -0,0 +1,30 @@
+package com.app.learning.execution;
+
+import java.io.File;
+
+import javax.servlet.ServletException;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+public class ServerLauncher {
+	public static void main(final String[] args) throws ServletException, LifecycleException {
+		String webappDirLocation = "src/main/webapp/";
+		Tomcat tomcat = new Tomcat();
+
+		//The port that we should run on can be set into an environment variable
+		//Look for that variable and default to 8080 if it isn't there.
+		String webPort = System.getenv("PORT");
+		if(webPort == null || webPort.isEmpty()) {
+			webPort = "8080";
+		}
+
+		tomcat.setPort(Integer.valueOf(webPort));
+
+		tomcat.addWebapp("/app", new File(webappDirLocation).getAbsolutePath());
+		System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
+
+		tomcat.start();
+		tomcat.getServer().await();
+	}
+}
